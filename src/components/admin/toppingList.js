900b283993941c8fabfe3cc659dbf95879dca563
Duynlph <duynlph27635@fpt.edu.vn>
2023-10-28 15:40:29 +0700

    test

---
+++ src/components/admin/toppingList.js
@@ -0,0 +1,21 @@
+const AdminToppingList = {
+    async render(toppingList) {
+        return /* html */`
+        <thead class="bg-gray-50">
+            <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Giá
+                </th>
+                <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        `;
+        export default AdminToppingList;
