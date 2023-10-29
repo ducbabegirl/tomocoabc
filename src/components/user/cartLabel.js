dd979c3f5209fbc5e63423b1b0267dba813f72b7
Vietanh <ngovietanh503@gmail.com>
2023-10-29 14:47:47 +0700

    update: icon cart

---
+++ src/components/user/cartLabel.js
@@ -0,0 +1,13 @@
+const CartLabel = {
+    render() {
+
+        return /* html */`
+        <a href="/#/cart" class="relative">
+            <label for="" class="absolute w-4 h-4 bg-green-700 text-xs text-center rounded-full -right-3 -top-1"></label>
+            <i class="fas fa-shopping-cart"></i>
+        </a>
+        `;
+    },
+};
+
+export default CartLabel;
