946610c0b7104e49a0d577aa3ab4003e61bac59a
ducbabegirl <ducmtph14090@fpt.edu.vn>
2023-10-29 21:36:35 +0700

    fixx_oder

--- src/api/order.js
+++ src/api/order.js
@@ -12,6 +12,7 @@ export const get = (id) => {
     const url = `/${TABLE_NAME}/${id}`;
     return instance.get(url);
 };
+
 export const add = (data) => {
     const url = `/${TABLE_NAME}`;
     return instance.post(url, data);
@@ -25,4 +26,21 @@ export const remove = (id) => {
 export const update = (id, data) => {
     const url = `/${TABLE_NAME}/${id}`;
     return instance.patch(url, data);
+};
+
+export const getByUserId = (userId, page, limit = 0) => {
+    let url = `/${TABLE_NAME}/?userId=${userId}&_sort=id&_order=desc`;
+    if (limit) url += `&_page=${page}&_limit=${limit}`;
+    return instance.get(url);
+};
+
+export const search = (key, stt, userId = 0) => {
+    let url = `/${TABLE_NAME}/?_sort=id&_order=desc`;
+    if (key) url += `&q=${key}`;
+
+    if (stt) url += `&status=${stt}`;
+
+    if (userId) url += `&userId=${userId}`;
+
+    return instance.get(url);
 };
