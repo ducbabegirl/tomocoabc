fd78bee0402c66a12c8b573c09dc0bb6b90ab9c0
Duynlph <duynlph27635@fpt.edu.vn>
2023-10-29 22:36:16 +0700

    logout admin

--- src/components/user/header.js
+++ src/components/user/header.js
@@ -2,10 +2,8 @@
 import toastr from "toastr";
 
 
-import { formatCurrency, getUser, reRender } from "../../utils";
-import Nav from "./nav";
 import CartLabel from "./cartLabel";
-=======
+
 import { search } from "../../api/product";
 import { formatCurrency, getUser, reRender } from "../../utils";
 import Nav from "./nav";
