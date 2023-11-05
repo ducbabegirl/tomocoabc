68a8160711e1c51f17cd07263aaa9d4b166fb630
ducbabegirl <ducmtph14090@fpt.edu.vn>
2023-11-05 18:40:24 +0700

    add-store

--- src/api/store.js
+++ src/api/store.js
@@ -7,3 +7,17 @@ export const getAll = (page, limit) => {
     if (limit) url += `&_page=${page}&_limit=${limit}`;
     return instance.get(url);
 };
+export const get = (id) => {
+    const url = `/${TABLE_NAME}/${id}`;
+    return instance.get(url);
+};
+
+export const add = (data) => {
+    const url = `/${TABLE_NAME}`;
+    return instance.post(url, data);
+};
+
+export const remove = (id) => {
+    const url = `/${TABLE_NAME}/${id}`;
+    return instance.delete(url);
+};
